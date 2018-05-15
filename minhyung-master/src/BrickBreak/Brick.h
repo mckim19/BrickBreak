effd2bf5a8537d7e4c54e8655d791a45b5e44767
HS Kim <disgaea3465@gmail.com>
2018-05-15 16:52:48 +0900

    BrickBreak: 벽돌 생성 테스트

---
+++ minhyung-master/src/BrickBreak/Brick.h
@@ -0,0 +1,11 @@
+#pragma once
+class Brick
+{
+public:
+	Brick();
+	~Brick();
+	int life;
+	int width, height;
+	int x1, x2, y1, y2;
+};
+
---
+++ minhyung-master/src/BrickBreak/brick.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "Ball.h"
+#include "Brick.h"
+
+
+Brick::Brick()
+{
+}
+
+
+Brick::~Brick()
+{
+}
